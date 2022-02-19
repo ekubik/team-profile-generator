0172c60e24647e562f612d1ce649e2a1905afd76
Ewa Kubik <ewa.kubik@hotmail.com>
2022-02-19 17:35:46 +1100

    created employee classes

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const jest = require('jest');
 
 const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
